6536aae2bd0073e478aa535ca18e43704da6d950
IsakHalll <isak.akerberg@elev.ga.ntig.se>
2024-08-21 11:08:29 +0200

    Dice sum probability calculator

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 
 def main():
     user_input = input().split(" ")
-
+    for i in range(int(user_input[0]+1)):
+        for x in range(int(user_input[1]+1)):
+            print (x)
+        
+        
 if __name__ == "__main__":
     main()
